30fb81b5587d5d91068feaf30991c401404e5501
toilet.js <toiletjs@users.noreply.github.com>
2016-07-23 12:10:39 -0700

    Correct a glaring oversight in the documentation.

--- fart.io.js
+++ fart.io.js
@@ -1,7 +1,7 @@
 /**
  * Farts.
  *
- * @param {boolean=} opt_isWet Whether the fart is wet.
+ * @param {boolean=} opt_isWet Whether the fart is wet. (Optional)
  */
 function fart(opt_isWet) {
   if (opt_isWet) {
