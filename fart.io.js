a1af147e14b03b24af18f5eaa9633d1eae09f136
toilet.js <toiletjs@users.noreply.github.com>
2016-07-23 12:07:54 -0700

    Create fart.io.js

---
+++ fart.io.js
@@ -0,0 +1,12 @@
+/**
+ * Farts.
+ *
+ * @param {boolean=} opt_isWet Whether the fart is wet.
+ */
+function fart(opt_isWet) {
+  if (opt_isWet) {
+    console.log('pfffffpfpfpffffff?');
+  } else {
+    console.log('poooOOOot.');
+  }
+}
